2a33af0c2dedb6c9f1ef0ac971974349f99308ac
schoeeva000 <schoeeva000@CAM-LAB-26.bgsd.k12.wa.us>
2019-09-16 07:15:32 -0700

    Name: Evan Schoening Date: 9/14/19 Mississippi assignment

--- src/Mississippi.java
+++ src/Mississippi.java
@@ -1,9 +1,97 @@
-
 public class Mississippi {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+  public static void main(String[] args) {
+    m();
+    i();
+    s();
+    s();
+    i();
+    s();
+    s();
+    i();
+    p();
+    p();
+    i();
+}
+  
 
-	}
+public static void m() {
+
+    System.out.println("M       M");
+    System.out.println("M M   M M");
+    System.out.println("M  M M  M");
+    System.out.println("M   M   M");
+    System.out.println("M       M");
+    System.out.println("M       M");
+    System.out.println("M       M");
+    System.out.println();
+}
+
+public static void i() {
+    System.out.println("IIIIIIIII");
+    System.out.println("    I");
+    System.out.println("    I");
+    System.out.println("    I");
+    System.out.println("    I");
+    System.out.println("    I");
+    System.out.println("IIIIIIIII");
+    System.out.println();
+}
+
+public static void s() {
+    System.out.println(" SSSSSSSS");
+    System.out.println("S");
+    System.out.println("S");
+    System.out.println(" SSSSSSS");
+    System.out.println("        S");
+    System.out.println("        S");
+    System.out.println("SSSSSSS");
+    System.out.println();
+}
+
+public static void p() {
+    System.out.println(" PPPPPPP");
+    System.out.println("P       P");
+    System.out.println("P       P");
+    System.out.println("PPPPPPPP");
+    System.out.println("P");
+    System.out.println("P");
+    System.out.println("P");
+    System.out.println();
+}
 
 }
+
+/*
+M       M
+M M   M M
+M  M M  M
+M   M   M
+M       M
+M       M
+M       M
+
+IIIIIIIII
+    I
+    I
+    I
+    I
+    I
+IIIIIIIII
+
+ SSSSSSSS
+S
+S
+ SSSSSSS
+        S
+        S
+SSSSSSSS
+
+ PPPPPPP
+P       P
+P       P
+PPPPPPPP
+P
+P
+P
+*/
